be58dffe9b0dc17638f82163477f42c7b465d99f
jonphilk <jonphilk@gmail.com>
2020-09-25 10:01:54 -0500

    Add project files.

---
+++ GarageExperts.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GarageExperts.WebMVC.Startup))]
+namespace GarageExperts.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
